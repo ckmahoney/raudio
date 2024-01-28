cc7e11db43de88ff8912c60b9ae45c5eeccb3ceb
ckmahoney <cortland.mahoney@gmail.com>
2024-01-28 16:33:17 -0500

    Add bin dir and files module

---
+++ src/files.rs
@@ -0,0 +1,16 @@
+use std::fs;
+use std::path::Path;
+
+pub fn exists(file_name:&str) -> Option<&str> {
+    if !fs::metadata(file_name).is_ok() {
+        return None 
+    }
+    return Some(file_name)
+}
+
+pub fn ensure_directory_exists(dir: &str) {
+    let path = Path::new(dir);
+    if !path.exists() {
+        fs::create_dir_all(path).expect("Failed to create directory");
+    }
+}
