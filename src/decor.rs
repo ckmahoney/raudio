03dd3aa0c3608bb23ce91fd4b99d4426980d9a85
ckmahoney <cortland.mahoney@gmail.com>
2024-04-24 13:23:46 -0400

    Fix freq mod val

--- src/decor.rs
+++ src/decor.rs
@@ -121,7 +121,7 @@ fn fmod(xyz:&Coords, ctx:&Ctx, snd:&Sound, dir:&Direction, phr:&Phrasing) -> f32
             1.005f32
         },
         timbre::Energy::High => {   
-            0.05f32
+            1.05f32
         }
     }
 }
