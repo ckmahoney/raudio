27de1afa5ad5ad3ec827baa7ae68bf9835146c47
ckmahoney <cortland.mahoney@gmail.com>
2024-05-12 13:06:47 -0400

    Add surge contour

--- src/phrasing/contour.rs
+++ src/phrasing/contour.rs
@@ -6,25 +6,53 @@ use crate::types::timbre::AmpContour;
 /// Given an index i in a sample buffer representing n_cycles,
 /// Produce amplitude modulation for a long form lifespan 
 /// Uses 1 and 0 as min and max values. May start and end on either.
-pub fn mod_contour(n_samples:usize, n_cycles:f32, contour:&AmpContour, rev:bool) -> AmpModulation {
+/// "Default" description is a forward contour (not reverse) starting at 0 going to 1.
+pub fn gen_contour(n_samples:usize, n_cycles:f32, contour:&AmpContour, reverse:bool) -> AmpModulation {
     use AmpContour::*;
     let mut modulator:AmpModulation = vec![0f32; n_samples];
 
     let range = modulator.iter_mut().enumerate();
 
+    let n = n_samples as f32;
+
     match contour {
+        // linear rise and fall
         Fade => {
-            for (i, sample) in range {
-                let p:f32 = i as f32 / n_samples as f32;
-                *sample = 1f32 - p
+            if reverse {
+                for (i, sample) in range {
+                    let p = i as f32 / n;
+                    *sample = 1f32 - p
+                }
+            } else {
+                for (i, sample) in range {
+                    let p = (n - i as f32) / n;
+                    *sample = 1f32 - p
+                }
             }
         },
+        Surge => {
+            let power:i32 = 5;
+            fn boost(p:f32) -> f32 {
+                (p/5f32)*(1f32-p).sqrt()
+            }
+            // rise and fall pow5 + logx boost
+            if reverse {    
+                for (i, sample) in range {
+                    let t = i as f32 / n;
+                    *sample = -(t - 1f32).powi(power) + boost(1f32-t);
+                }
+            } else {
+                for (i, sample) in range {
+                    let t = i as f32 / n;
+                    *sample = t.powi(power) + boost(t);
+                }
+            }
+        }
         _ => {
             panic!("Not implemented for contour")
         }
     };
 
-    if rev { modulator.reverse() };
     modulator
 }
 
@@ -55,7 +83,53 @@ fn apply_contour(signal: &mut SampleBuffer, contour:&AmpModulation) {
 
 
 #[cfg(test)]
-mod test {
+mod test_gen_contour {
+
+    use super::*;
+    static n_samples:usize = 48000 * 1;
+    static n_cycles:f32 = 2f32;
+    static err_margin:f32 = 0.0001;
+
+    #[test]
+    fn test_contour_fade() {
+        let amp_mod = gen_contour(n_samples, n_cycles, &AmpContour::Fade, false);
+        assert_eq!(amp_mod.len(), n_samples, "Must produce a contour of the requested duration in cycles");
+        assert_eq!(amp_mod[0], 0f32, "Linear fade must start at 0");
+
+        let f:f32 = amp_mod[amp_mod.len()-1];
+        assert!((1f32 - f) < err_margin, "Linear fade must end near 1 when reversed");
+
+        let amp_mod = gen_contour(n_samples, n_cycles, &AmpContour::Fade, true);
+        assert_eq!(amp_mod.len(), n_samples, "Must produce a reversed contour of the requested duration in cycles");
+        
+        let f:f32 = amp_mod[0];
+        assert!(f == 1f32, "Must start with 1 when using reverse linear fade");
+        let v:f32 = amp_mod[amp_mod.len()-1];
+        assert!(v <= err_margin, "Must have a final value near 0");
+    }
+
+
+    #[test]
+    fn test_contour_surge() {
+        let amp_mod = gen_contour(n_samples, n_cycles, &AmpContour::Surge, false);
+        assert_eq!(amp_mod.len(), n_samples, "Must produce a contour of the requested duration in cycles");
+        assert_eq!(amp_mod[0], 0f32, "Polynomial surge must start at 1. Found {:#?}", &amp_mod[0..10]);
+
+        let f:f32 = amp_mod[amp_mod.len()-1];
+        assert!((1f32 - f) <= err_margin, "Must have a final value near 1");
+
+        let amp_mod = gen_contour(n_samples, n_cycles, &AmpContour::Surge, true);
+        assert_eq!(amp_mod.len(), n_samples, "Must produce a reversed contour of the requested duration in cycles");
+        
+        let f:f32 = amp_mod[0];
+        assert!((1f32 - f) <= err_margin, "Must start near 1 when using reverse linear surge");
+        let v:f32 = amp_mod[amp_mod.len()-1];
+        assert!(v < err_margin, "Linear surge must end near 0 when reversed");
+    }
+}
+
+#[cfg(test)]
+mod test_apply_contour {
     use super::*;
     use crate::analysis;
     static min_allowed_mod:f32 = 0f32;
