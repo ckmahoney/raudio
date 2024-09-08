f360b0e62c9d610aa01b316cbcb2b3f96697e380
ckmahoney <cortland.mahoney@gmail.com>
2024-09-08 13:19:43 -0400

    Add burp amod

--- src/phrasing/ranger.rs
+++ src/phrasing/ranger.rs
@@ -20,10 +20,13 @@ pub struct Knob {
     pub c: Range,
 }
 
-/// Signature:
-/// knob, cps, fundamental, multiplier, n_cycles, pos_cycles -> modulation value
+/// A dynamically dispatched callback for modulating amplitude OR frequency OR phase OR time.  
+/// Signature:  
+/// `knob, cps, fundamental, multiplier, n_cycles, pos_cycles` -> `modulation value`
 pub type Ranger  = fn(&Knob, f32, f32, f32, f32, f32) -> f32;
+
 pub type KnobbedRanger = (Knob, Ranger);
+
 #[derive(Clone,Debug)]
 pub struct KnobMods (pub Vec<KnobbedRanger>, pub Vec<KnobbedRanger>, pub Vec<KnobbedRanger>);
 impl KnobMods {
@@ -189,36 +192,6 @@ pub fn pmod_chorus(knob: &Knob, cps: f32, fund: f32, mul: f32, n_cycles: f32, po
 }
 
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use crate::analysis;
-
-    static n_samples:usize = 1000;
-    static cps:f32 = 1.4;
-    static fund:f32 = 1.9;
-    static muls:[f32;4] = [1f32, 10f32, 20f32, 200f32];
-    static n_cycles:f32= 1.5f32;
-
-    #[test]
-    fn test_fmod_sweepdown_a1_b0() {  
-        for &mul in &muls {
-            let mut signal:Vec<f32> = Vec::with_capacity(n_samples);
-            let knob:Knob = Knob { a:1f32, b:0f32, c:0f32};
-            // Generate a buffer of modulation envelope data 
-            for i in 0..n_samples {
-                let pos_cycles = n_cycles * (i as f32/ n_samples as f32);
-                signal.push(fmod_sweepdown(&knob, cps, fund, mul, n_cycles, pos_cycles))
-            }
-            let f:f32 = mul * fund;
-            let mut bads:Vec<(usize,f32)> = Vec::new();
-
-            assert!(signal.iter().all(|&v| analysis::is_fmod_range(f, v)), "fmod_sweepdown must only produce valid frequency modulation results");
-            assert!(analysis::is_monotonically_decreasing(&signal), "fmod_sweepdown must produce only values that are constantly decreasing.");
-        }
-    }
-}
-
 
 /// A oneshot amplitude modulation adding a "huh" like breathing in for a word.
 ///
@@ -290,12 +263,9 @@ pub fn amod_impulse(knob: &Knob, cps: f32, fund: f32, mul: f32, n_cycles: f32, p
 /// 
 /// ## Knob Params
 /// 
-/// `a`: Base decay rate. Value of 0 is the shortest possible base decay, and 1 is the longest.
-/// `b`: Upper harmonic decay sensitivity. Value of 0 means all harmonics have same decay. Value of 1 means later harmonics decay much more rapidly than early harmonics. 
-/// `b`: unused.   
+/// `a`: Base decay rate. Value of 0 is the shortest possible base decay, and 1 is the longest.  
+/// `b`: Upper harmonic decay sensitivity. Value of 0 means all harmonics have same decay. Value of 1 means later harmonics decay much more rapidly than early harmonics.  
 /// `c`: unused.  
-/// 
-/// ## Observations
 /// 
 /// ## Desmos 
 /// https://www.desmos.com/calculator/jw8vzd2ie1
@@ -310,6 +280,33 @@ pub fn amod_pluck(knob: &Knob, cps: f32, fund: f32, mul: f32, n_cycles: f32, pos
     1f32/(decay_rate*t).exp()
 }
 
+/// A oneshot amplitdue modulation for slow decay.
+///
+/// ## Arguments
+/// `cps` Instantaneous playback rate as cycles per second  
+/// `fund` The reference fundamental frequency  
+/// `mul` The current multiplier wrt the fundamental  
+/// `n_cycles` Total duration of this event in cycles  
+/// `pos_cycles` The current position in the event (in cycles)  
+/// 
+/// ## Knob Params
+/// 
+/// `a`: Base decay rate. Value of 0 has the least overall volume. Value of 1 has the most overall volume.   
+/// `b`: Upper harmonic stretch. Value of 0 means all multipliers have the same decay rate. Value of 1 adds tiny volume as mul increases. Has more prominent effect for low a values. For a=1, is nearly invisible.
+/// `c`: unused.  
+/// 
+/// ## Desmos 
+/// https://www.desmos.com/calculator/4gc7quzfvw
+/// 
+/// ## Returns
+pub fn amod_burp(knob: &Knob, cps: f32, fund: f32, mul: f32, n_cycles: f32, pos_cycles: f32) -> f32 {
+    let max_mul: f32 = NFf / (mul * fund);
+    let t:f32 = pos_cycles/n_cycles;
+    let base_decay_rate:f32 = 2f32 + (8f32 * knob.a);
+    let decay_mod_add:f32 = 2f32 * knob.b * mul/max_mul;
+    1f32 - t.powf(base_decay_rate + decay_mod_add)
+}
+
 /// A continuous amplitdue modulation for periodic falling linear contour.
 ///
 /// ## Arguments
@@ -321,11 +318,9 @@ pub fn amod_pluck(knob: &Knob, cps: f32, fund: f32, mul: f32, n_cycles: f32, pos
 /// 
 /// ## Knob Params
 /// 
-/// `a`: Tremelo period. Value of 0 is 1 hits per note, and value of 1 is 2^4 hits per note.
+/// `a`: Tremelo period. Value of 0 is 1 hits per note, and value of 1 is 2^4 hits per note.  
 /// `b`: unused.   
 /// `c`: unused.  
-/// 
-/// ## Observations
 /// 
 /// ## Desmos 
 /// https://www.desmos.com/calculator/2bf9xonkci
@@ -350,11 +345,9 @@ pub fn amod_oscillation_tri(knob: &Knob, cps: f32, fund: f32, mul: f32, n_cycles
 /// 
 /// ## Knob Params
 /// 
-/// `a`: Tremelo period. Value of 0 is 1 hits per note, and value of 1 is 2^4 hits per note.
+/// `a`: Tremelo period. Value of 0 is 1 hits per note, and value of 1 is 2^4 hits per note.  
 /// `b`: unused.   
 /// `c`: unused.  
-/// 
-/// ## Observations
 /// 
 /// ## Desmos 
 /// https://www.desmos.com/calculator/ag0vtteimu
@@ -365,4 +358,36 @@ pub fn amod_oscillation_sine(knob: &Knob, cps: f32, fund: f32, mul: f32, n_cycle
     let osc_rate:f32 = 2f32.powf(-4f32 * knob.a);
     let osc_mod_mul:f32 = 2f32.powf(knob.a * 4f32);
     1f32 - (pi * t * osc_mod_mul).sin().abs().powi(4i32)
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::analysis;
+
+    static n_samples:usize = 1000;
+    static cps:f32 = 1.4;
+    static fund:f32 = 1.9;
+    static muls:[f32;4] = [1f32, 10f32, 20f32, 200f32];
+    static n_cycles:f32= 1.5f32;
+
+    #[test]
+    fn test_fmod_sweepdown_a1_b0() {  
+        for &mul in &muls {
+            let mut signal:Vec<f32> = Vec::with_capacity(n_samples);
+            let knob:Knob = Knob { a:1f32, b:0f32, c:0f32};
+            // Generate a buffer of modulation envelope data 
+            for i in 0..n_samples {
+                let pos_cycles = n_cycles * (i as f32/ n_samples as f32);
+                signal.push(fmod_sweepdown(&knob, cps, fund, mul, n_cycles, pos_cycles))
+            }
+            let f:f32 = mul * fund;
+            let mut bads:Vec<(usize,f32)> = Vec::new();
+
+            assert!(signal.iter().all(|&v| analysis::is_fmod_range(f, v)), "fmod_sweepdown must only produce valid frequency modulation results");
+            assert!(analysis::is_monotonically_decreasing(&signal), "fmod_sweepdown must produce only values that are constantly decreasing.");
+        }
+    }
 }
