bc884fdeb783c7819f55e0c4999d5e5f85ac3786
ckmahoney <cortland.mahoney@gmail.com>
2024-09-08 17:53:34 -0400

    Add copy of lead as chords to demo

--- src/demo/kuwuku.rs
+++ src/demo/kuwuku.rs
@@ -15,9 +15,9 @@ use presets::kuwuku::{vibe, sine, brush, kick};
 
 fn sine_melody() -> Melody<Note> {
     let tala:Vec<Duration> = vec![
-        (1i32,1i32), 
-        (3i32,4i32),
-        (5i32,4i32),
+        (1i32, 1i32), 
+        (3i32, 4i32),
+        (5i32, 4i32),
         (1i32, 1i32),
         (1i32, 2i32),
         (1i32, 2i32),
@@ -46,6 +46,46 @@ fn sine_melody() -> Melody<Note> {
     ]
 }
 
+fn chords_melody() -> Melody<Note> {
+    let tala:Vec<Duration> = vec![
+        (2i32, 1i32), 
+        (3i32, 1i32),
+        (2i32, 1i32), 
+        (1i32, 1i32), 
+    ];
+
+    let amps:Vec<Ampl> = vec![
+        1f32, 0.66f32, 0.66f32, 1f32,
+    ].iter().map(|x| x * db_to_amp(-6f32)).collect::<Vec<f32>>();
+
+    let line_1:Vec<Tone> = vec![
+        (8, (0i8, 0i8, 1i8)),
+        (8, (-1i8, 0i8, 1i8)),
+        (8, (0i8, 0i8, 1i8)),
+        (8, (1i8, 0i8, 1i8)),
+    ];
+
+    let line_2:Vec<Tone> = vec![
+        (8, (0i8, 0i8, 3i8)),
+        (8, (-1i8, 0i8, 3i8)),
+        (8, (0i8, 0i8, 3i8)),
+        (8, (1i8, 0i8, 3i8)),
+    ];
+
+    let line_3:Vec<Tone> = vec![
+        (8, (0i8, 0i8, 5i8)),
+        (8, (-1i8, 0i8, 5i8)),
+        (8, (0i8, 0i8, 5i8)),
+        (8, (1i8, 0i8, 5i8)),
+    ];
+
+    vec![
+        zip_line(tala.clone(), line_1, amps.clone()),
+        zip_line(tala.clone(), line_2, amps.clone()),
+        zip_line(tala.clone(), line_3, amps.clone()),
+    ]
+}
+
 fn kick_melody() -> Melody<Note> {
     let tala:Vec<Duration> = vec![
         (1i32,1i32), 
@@ -61,23 +101,74 @@ fn kick_melody() -> Melody<Note> {
     let amps:Vec<Ampl> = vec![
         1f32, 0.66f32, 1f32, 
         1f32, 0.5f32, 0.75f32, 1f32, 0.66f32
-    ].iter().map(|x| x * db_to_amp(-3f32)).collect::<Vec<f32>>();
+    ].iter().map(|x| x * db_to_amp(-12f32)).collect::<Vec<f32>>();
 
     let tones:Vec<Tone> = vec![
         (5, (0i8, 0i8, 1i8)),
-        (5, (0i8, 0i8, 5i8)),
-        (5, (0i8, 0i8, 3i8)),
-        (5, (1i8, 0i8, 1i8)),
-        (5, (1i8, 0i8, 5i8)),
-        (5, (1i8, 0i8, 3i8)),
-        (5, (-1i8, 0i8, 5i8)),
-        (5, (-1i8, 0i8, 1i8)),
+        (5, (0i8, 0i8, 1i8)),
+        (5, (0i8, 0i8, 1i8)),
+        (5, (0i8, 0i8, 1i8)),
+        (5, (0i8, 0i8, 1i8)),
+        (5, (0i8, 0i8, 1i8)),
+        (5, (0i8, 0i8, 1i8)),
+        (5, (0i8, 0i8, 1i8)),
     ];
 
     vec![
         zip_line(tala, tones, amps)
     ]
 }
+
+
+fn hat_melody() -> Melody<Note> {
+    let tala:Vec<Duration> = vec![
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (-1i32,2i32), 
+        (1i32,2i32),
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (1i32,2i32), 
+        (-1i32,2i32), 
+        (1i32,2i32), 
+        (-1i32,2i32), 
+        (1i32,2i32), 
+    ];
+
+    let amps:Vec<Ampl> = vec![
+        0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32,
+        0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32, 0.5f32,
+    ].iter().map(|x| x * db_to_amp(-24f32)).collect::<Vec<f32>>();
+
+    let tones:Vec<Tone> = vec![
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+        (10, (0i8, 0i8, 1i8)),
+    ];
+
+    vec![
+        zip_line(tala, tones, amps)
+    ]
+}
+
 
 fn brush_melody() -> Melody<Note> {
     let tala:Vec<Duration> = vec![
@@ -155,6 +246,18 @@ fn sine_arf() -> Arf {
 }
 
 
+fn chords_arf() -> Arf {
+    Arf {
+        mode: Mode::Melodic,
+        role: Role::Chords,
+        register: 8,
+        visibility: Visibility::Background,
+        energy: Energy::Medium,
+        presence: Presence::Tenuto,
+    }
+}
+
+
 fn kick_arf() -> Arf {
     Arf {
         mode: Mode::Enharmonic,
@@ -200,6 +303,7 @@ fn demonstrate() {
     let root:f32 = 1.2;
     let labels:Vec<&str> = vec!["vibe", "sine", "brush"];
     let ely_vibe = vibe::driad(&vibe_arf());
+    let ely_chords = vibe::driad(&vibe_arf());
     let ely_brush = brush::driad(&brush_arf());
     let ely_sine = sine::driad(&sine_arf());
     let expr:Expr = (vec![1f32], vec![1f32], vec![0f32]);
@@ -218,9 +322,15 @@ fn demonstrate() {
         modifiers: ely_sine.modders,
         clippers: (0f32, 1f32)
     };
+    let feel_chords:Feel = Feel {
+        bp: (vec![MFf], vec![NFf]),
+        modifiers: ely_chords.modders,
+        clippers: (0f32, 1f32)
+    };
     let delays:Vec<DelayParams> = vec![delay::passthrough];
 
     let stem_vibe =(&vibe_melody(), ely_vibe.soids, vibe::expr(&vibe_arf()), feel_vibe, ely_vibe.knob_mods, vec![delay::passthrough]);
+    let stem_chords =(&chords_melody(), ely_chords.soids, vibe::expr(&vibe_arf()), feel_chords, ely_chords.knob_mods, vec![delay::passthrough]);
     let stem_sine = (&sine_melody(), ely_sine.soids, expr, feel_sine, ely_sine.knob_mods, vec![delay::passthrough]);
     let stem_brush = (&brush_melody(), ely_brush.soids, brush::expr(&brush_arf()), feel_brush, ely_brush.knob_mods, vec![delay::passthrough]);
     let kick_mel = kick_melody();
@@ -231,6 +341,7 @@ fn demonstrate() {
         Instance(stem_vibe),
         Instance(stem_sine),
         Instance(stem_brush),
+        Instance(stem_chords),
         Group(group_kick)
     ];
 
